711f7bb0fede1ae1ae7a69fd38b3047968335463
bilalwebdev26 <f2022266833@umt.edu.pk>
2025-10-30 08:13:02 +0500

    test

--- services/user-service/dist/server.js
+++ services/user-service/dist/server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 const app = express();
 dotenv.config();
+app.get("/",(req,res)=>{
+  res.send("hey")
+})
 const PORT = process.env.PORT || 3006;
 app.listen(PORT, () => {
   console.log(`User Service run : ${PORT}`);
