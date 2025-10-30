9ee2ae3ab34f62d02834b0ca212dad3cf0800757
bilalwebdev26 <f2022266833@umt.edu.pk>
2025-10-30 08:32:10 +0500

    test3

--- services/user-service/dist/server.js
+++ services/user-service/dist/server.js
@@ -8,6 +8,9 @@ app.get("/",(req,res)=>{
 app.post("/",(req,res)=>{
   res.send("hey")
 })
+app.put("/",(req,res)=>{
+  res.send("hey")
+})
 const PORT = process.env.PORT || 3006;
 app.listen(PORT, () => {
   console.log(`User Service run : ${PORT}`);
