bc27c9607bd5ad914e693e0007b6caf0eb976001
bilalwebdev26 <f2022266833@umt.edu.pk>
2025-10-31 20:41:19 +0500

    Update user

--- services/user-service/dist/server.js
+++ services/user-service/dist/server.js
@@ -2,15 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 const app = express();
 dotenv.config();
-app.get("/",(req,res)=>{
-  res.send("hey")
-})
-app.post("/",(req,res)=>{
-  res.send("hey")
-})
-app.put("/",(req,res)=>{
-  res.send("hey")
-})
+app.get('/', (req, res) => {
+  res.send('hey');
+});
+app.post('/', (req, res) => {
+  res.send('hey');
+});
+app.put('/', (req, res) => {
+  res.send('hey');
+});
 const PORT = process.env.PORT || 3006;
 app.listen(PORT, () => {
   console.log(`User Service run : ${PORT}`);
