79325e6f7980508db672fd7c07e2b922871b9c04
bilalwebdev26 <f2022266833@umt.edu.pk>
2025-11-01 00:23:59 +0500

    course check

--- services/course-service/src/server.ts
+++ services/course-service/src/server.ts
@@ -27,38 +27,38 @@
 //   });
 
 import express from 'express';
-<<<<<<< HEAD
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import teacherCourseRoutes from './routes/teacher.course.js';
-import studentCourseRoutes from './routes/student.course.js';
-import adminCourseRoutes from './routes/admin.course.js';
-import { connectDB } from './config/db.config.js';
-const unused = "ali"
-const unused2 = "ali1"
-dotenv.config();
-const app = express();
-const PORT = process.env.PORT || 3003;
-//-------------middlewares----
-app.use(express.json());
-app.use(cookieParser());
-//-------------Routes.........
-app.use('/api/v1/teacher/course', teacherCourseRoutes);
-app.use('/api/v1/admin/course', adminCourseRoutes);
-app.use('/api/v1//studentcourse', studentCourseRoutes);
-//----------------------------
-connectDB()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`App running : ${unused} ${unused2}`, PORT);
-    });
-  })
-  .catch((error) => {
-    console.log('Error occur while server running : ', error);
-  });
-=======
+// <<<<<<< HEAD
+// import dotenv from 'dotenv';
+// import cookieParser from 'cookie-parser';
+// import teacherCourseRoutes from './routes/teacher.course.js';
+// import studentCourseRoutes from './routes/student.course.js';
+// import adminCourseRoutes from './routes/admin.course.js';
+// import { connectDB } from './config/db.config.js';
+// const unused = "ali"
+// const unused2 = "ali1"
+// dotenv.config();
+// const app = express();
+// const PORT = process.env.PORT || 3003;
+// //-------------middlewares----
+// app.use(express.json());
+// app.use(cookieParser());
+// //-------------Routes.........
+// app.use('/api/v1/teacher/course', teacherCourseRoutes);
+// app.use('/api/v1/admin/course', adminCourseRoutes);
+// app.use('/api/v1//studentcourse', studentCourseRoutes);
+// //----------------------------
+// connectDB()
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`App running : ${unused} ${unused2}`, PORT);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log('Error occur while server running : ', error);
+//   });
+// =======
 const app = express();
 app.listen(3003, () => {
   console.log('Course App running : ', 3003);
 });
->>>>>>> main
+// >>>>>>> main
